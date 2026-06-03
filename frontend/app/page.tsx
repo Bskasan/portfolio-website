147261bb397a5806e0da91a33c118a0f60315fa4
Bekir Kasan <53233822+Bskasan@users.noreply.github.com>
2026-06-03 17:17:58 +0300

    Set font as roboto mono as designed

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,9 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans">
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50">
       <main className="flex flex-1 w-full max-w-6xl flex-col items-center justify-between py-32 px-16 bg-zinc-50 sm:items-start">
-        
+        <p>
+          This is the home page.
+        </p>
       </main>
     </div>
   );
