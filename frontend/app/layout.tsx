7df445b6fb9f7c0193c62d7e1bbe2236f2d8858d
Bekir Kasan <53233822+Bskasan@users.noreply.github.com>
2026-06-18 16:15:39 +0300

    google analytics setup with next.js

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,6 +3,7 @@ import { Roboto_Mono } from "next/font/google";
 import "../styles/globals.css";
 import { ViewTransitions } from "next-view-transitions";
 import NavBar from "@/components/navbar/NavBar";
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 
 const robotoMono = Roboto_Mono({
   subsets: ["latin"],
@@ -29,6 +30,8 @@ export default function RootLayout({
         <body className="min-h-full flex flex-col font-mono">
           <NavBar />
           <main>{children}</main>
+          <GoogleAnalytics gaId="G-VP78F2NX1H" />
+          <GoogleTagManager gtmId="G-VP78F2NX1H" />
         </body>
       </html>
     </ViewTransitions>
